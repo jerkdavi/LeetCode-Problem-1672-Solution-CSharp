e097ae16aeb575a3b3fa4bae7938bad0089935f6
David Jerković <jerkdavi@gmail.com>
2024-06-26 17:51:55 +0200

    Updated Program class - basic 2D array initialization

--- RichestCustomerWealth/Program.cs
+++ RichestCustomerWealth/Program.cs
@@ -7,18 +7,18 @@ namespace RichestCustomerWealth
         static void Main(string[] args)
         {
             //1672.
-            //int[][] accounts = new int[][]
+            //int[][] accounts =
             //{
             //    new int[] { 1, 2, 3 },
             //    new int[] { 3, 2, 1 }
             //};
-            //int[][] accounts = new int[][]
+            //int[][] accounts =
             //{
             //    new int[] { 1, 5 },
             //    new int[] { 7, 3 },
             //    new int[] { 3, 5 }
             //};
-            //int[][] accounts = new int[][]
+            //int[][] accounts =
             //{
             //    new int[] { 2, 8, 7 },
             //    new int[] { 7, 1, 3 },
@@ -29,10 +29,10 @@ namespace RichestCustomerWealth
             Console.Write("What is the number of values: ");
             int valuesLength = int.Parse(Console.ReadLine());
             int[][] accounts = new int[accountsLength][];
-            for(int i = 0; i < accountsLength; i++)
+            for (int i = 0; i < accountsLength; i++)
             {
                 accounts[i] = new int[valuesLength];
-                for(int j = 0; j < valuesLength; j++)
+                for (int j = 0; j < valuesLength; j++)
                 {
                     Console.Write($"Enter account[{i + 1}][{j + 1}] value: ");
                     accounts[i][j] = int.Parse(Console.ReadLine());
